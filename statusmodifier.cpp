1e1b3afbce012a20d541eafab0536920906b0c0f
Sachin <sachinfnatic@gmail.com>
2025-12-08 12:50:52 +0530

    fix: recompile after gameupdate

--- statusmodifier.cpp
+++ statusmodifier.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "statusmodifier.h"
 #include "metamod_oslink.h"
 #include "schemasystem/schemasystem.h"
 #include "vprof.h"
+#include <stdio.h>
 
 #include "protobuf/generated/cstrike15_usermessages.pb.h"
 
@@ -22,7 +22,8 @@ bool g_bHasTicked;
 
 std::unordered_set<int> g_mExcludeSlots;
 
-void (*StatusPrintClient_t)(CServerSideClientBase *pPlayer, bool verbose, CBufferString *clients) = nullptr;
+void (*StatusPrintClient_t)(CServerSideClientBase *pPlayer, bool verbose,
+							CBufferString *clients) = nullptr;
 
 using namespace DynLibUtils;
 
@@ -35,31 +36,34 @@ CGameEntitySystem *GameEntitySystem()
 #else
 	static int offset = 80;
 #endif
-	return *reinterpret_cast<CGameEntitySystem **>((uintptr_t)(g_pGameResourceServiceServer) + offset);
+	return *reinterpret_cast<CGameEntitySystem **>(
+		(uintptr_t)(g_pGameResourceServiceServer) + offset);
 }
 
 // Will return null between map end & new map startup, null check if necessary!
-CGlobalVars *GetGlobals()
-{
-	return engine->GetServerGlobals();
-}
+CGlobalVars *GetGlobals() { return engine->GetServerGlobals(); }
 
 class GameSessionConfiguration_t
 {
 };
 
-SH_DECL_HOOK3_void(INetworkServerService, StartupServer, SH_NOATTRIB, 0, const GameSessionConfiguration_t &, ISource2WorldSession *, const char *);
+SH_DECL_HOOK3_void(INetworkServerService, StartupServer, SH_NOATTRIB, 0,
+				   const GameSessionConfiguration_t &, ISource2WorldSession *,
+				   const char *);
 SH_DECL_HOOK3_void(IServerGameDLL, GameFrame, SH_NOATTRIB, 0, bool, bool, bool);
 
-void FASTCALL Hook_StatusPrintClient(CServerSideClientBase *pPlayer, bool verbose, CBufferString *clients)
+void FASTCALL Hook_StatusPrintClient(CServerSideClientBase *pPlayer,
+									 bool verbose, CBufferString *clients)
 {
-	if (!pPlayer || pPlayer->IsFakeClient() || std::string(pPlayer->GetClientName()).empty())
+	if (!pPlayer || pPlayer->IsFakeClient() ||
+		std::string(pPlayer->GetClientName()).empty())
 	{
 		StatusPrintClient_t(pPlayer, verbose, clients);
 		return;
 	}
 
-	if (g_mExcludeSlots.find(pPlayer->GetPlayerSlot().Get()) != g_mExcludeSlots.end())
+	if (g_mExcludeSlots.find(pPlayer->GetPlayerSlot().Get()) !=
+		g_mExcludeSlots.end())
 	{
 		return;
 	}
@@ -67,40 +71,56 @@ void FASTCALL Hook_StatusPrintClient(CServerSideClientBase *pPlayer, bool verbos
 	StatusPrintClient_t(pPlayer, verbose, clients);
 }
 
-bool StatusModifier::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late)
+bool StatusModifier::Load(PluginId id, ISmmAPI *ismm, char *error,
+						  size_t maxlen, bool late)
 {
 	PLUGIN_SAVEVARS();
 
 	GET_V_IFACE_CURRENT(GetEngineFactory, g_pCVar, ICvar, CVAR_INTERFACE_VERSION);
-	GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer2, SOURCE2ENGINETOSERVER_INTERFACE_VERSION);
-	GET_V_IFACE_CURRENT(GetFileSystemFactory, g_pFullFileSystem, IFileSystem, FILESYSTEM_INTERFACE_VERSION);
-	GET_V_IFACE_CURRENT(GetEngineFactory, g_pGameResourceServiceServer, IGameResourceService, GAMERESOURCESERVICESERVER_INTERFACE_VERSION);
-	GET_V_IFACE_ANY(GetEngineFactory, g_pSchemaSystem, ISchemaSystem, SCHEMASYSTEM_INTERFACE_VERSION);
-	GET_V_IFACE_ANY(GetServerFactory, g_pSource2Server, ISource2Server, SOURCE2SERVER_INTERFACE_VERSION);
-	GET_V_IFACE_ANY(GetEngineFactory, g_pNetworkServerService, INetworkServerService, NETWORKSERVERSERVICE_INTERFACE_VERSION);
+	GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer2,
+						SOURCE2ENGINETOSERVER_INTERFACE_VERSION);
+	GET_V_IFACE_CURRENT(GetFileSystemFactory, g_pFullFileSystem, IFileSystem,
+						FILESYSTEM_INTERFACE_VERSION);
+	GET_V_IFACE_CURRENT(GetEngineFactory, g_pGameResourceServiceServer,
+						IGameResourceService,
+						GAMERESOURCESERVICESERVER_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetEngineFactory, g_pSchemaSystem, ISchemaSystem,
+					SCHEMASYSTEM_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetServerFactory, g_pSource2Server, ISource2Server,
+					SOURCE2SERVER_INTERFACE_VERSION);
+	GET_V_IFACE_ANY(GetEngineFactory, g_pNetworkServerService,
+					INetworkServerService,
+					NETWORKSERVERSERVICE_INTERFACE_VERSION);
 
-	SH_ADD_HOOK(INetworkServerService, StartupServer, g_pNetworkServerService, SH_MEMBER(this, &StatusModifier::Hook_StartupServer), true);
-	SH_ADD_HOOK(IServerGameDLL, GameFrame, g_pSource2Server, SH_MEMBER(this, &StatusModifier::Hook_GameFrame), true);
+	SH_ADD_HOOK(INetworkServerService, StartupServer, g_pNetworkServerService,
+				SH_MEMBER(this, &StatusModifier::Hook_StartupServer), true);
+	SH_ADD_HOOK(IServerGameDLL, GameFrame, g_pSource2Server,
+				SH_MEMBER(this, &StatusModifier::Hook_GameFrame), true);
 
 	CModule libengine(engine);
 
-	// search for "challenging" in engine2.dll or libengine2.so
+	// search for "challenging" in engine2.dll or libengine2.so. Go to the top
 #ifdef _WIN32
-	const char *szSignature = "48 8B C4 55 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 48 89 58 ? 49 8B D8";
+	const char *szSignature =
+		"48 8B C4 55 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 48 89 58 ? 49 8B D8";
 #else
-	const char *szSignature = "55 48 89 E5 41 57 41 56 4C 8D 3D ? ? ? ? 41 55 41 54 49 89 D4";
+	const char *szSignature =
+		"55 48 89 E5 41 57 41 56 4C 8D 3D ? ? ? ? 41 55 41 54 49 89 D4";
 #endif
 
-	StatusPrintClient_t = libengine.FindPattern(szSignature).RCast<decltype(StatusPrintClient_t)>();
+	StatusPrintClient_t =
+		libengine.FindPattern(szSignature).RCast<decltype(StatusPrintClient_t)>();
 	if (!StatusPrintClient_t)
 	{
-		ErrorLog("[%s] Failed to find function to get StatusPrint of client", g_PLAPI->GetLogTag());
+		ErrorLog("[%s] Failed to find function to get StatusPrint of client",
+				 g_PLAPI->GetLogTag());
 		return false;
 	}
 	else
 	{
 		m_StatusHook = funchook_create();
-		funchook_prepare(m_StatusHook, (void **)&StatusPrintClient_t, (void *)Hook_StatusPrintClient);
+		funchook_prepare(m_StatusHook, (void **)&StatusPrintClient_t,
+						 (void *)Hook_StatusPrintClient);
 		funchook_install(m_StatusHook, 0);
 		ConMsg("[StatusModifier] StatusPrint of client hooked successfully.\n");
 	}
@@ -121,8 +141,10 @@ bool StatusModifier::Unload(char *error, size_t maxlen)
 {
 	ConVar_Unregister();
 
-	SH_REMOVE_HOOK(INetworkServerService, StartupServer, g_pNetworkServerService, SH_MEMBER(this, &StatusModifier::Hook_StartupServer), true);
-	SH_REMOVE_HOOK(IServerGameDLL, GameFrame, g_pSource2Server, SH_MEMBER(this, &StatusModifier::Hook_GameFrame), true);
+	SH_REMOVE_HOOK(INetworkServerService, StartupServer, g_pNetworkServerService,
+				   SH_MEMBER(this, &StatusModifier::Hook_StartupServer), true);
+	SH_REMOVE_HOOK(IServerGameDLL, GameFrame, g_pSource2Server,
+				   SH_MEMBER(this, &StatusModifier::Hook_GameFrame), true);
 
 	if (m_StatusHook)
 		funchook_destroy(m_StatusHook);
@@ -132,7 +154,9 @@ bool StatusModifier::Unload(char *error, size_t maxlen)
 	return true;
 }
 
-void StatusModifier::Hook_StartupServer(const GameSessionConfiguration_t &config, ISource2WorldSession *pSession, const char *pszMapName)
+void StatusModifier::Hook_StartupServer(
+	const GameSessionConfiguration_t &config, ISource2WorldSession *pSession,
+	const char *pszMapName)
 {
 	g_pEntitySystem = GameEntitySystem();
 	g_bHasTicked = false;
@@ -140,7 +164,8 @@ void StatusModifier::Hook_StartupServer(const GameSessionConfiguration_t &config
 	g_mExcludeSlots.clear();
 }
 
-void StatusModifier::Hook_GameFrame(bool simulating, bool bFirstTick, bool bLastTick)
+void StatusModifier::Hook_GameFrame(bool simulating, bool bFirstTick,
+									bool bLastTick)
 {
 	/**
 	 * simulating:
@@ -188,7 +213,8 @@ void StatusModifier::Hook_GameFrame(bool simulating, bool bFirstTick, bool bLast
 	}
 }
 
-CON_COMMAND_F(mm_excludeslot, "Exclude slot from status", FCVAR_SERVER_CAN_EXECUTE)
+CON_COMMAND_F(mm_excludeslot, "Exclude slot from status",
+			  FCVAR_SERVER_CAN_EXECUTE)
 {
 	if (context.GetPlayerSlot() != -1)
 		return;
@@ -205,7 +231,8 @@ CON_COMMAND_F(mm_excludeslot, "Exclude slot from status", FCVAR_SERVER_CAN_EXECU
 	ConMsg("[StatusModifier] Added slot %d to exclude list.\n", slot);
 }
 
-CON_COMMAND_F(mm_removeexcludeslot, "Remove exclusion from status", FCVAR_SERVER_CAN_EXECUTE)
+CON_COMMAND_F(mm_removeexcludeslot, "Remove exclusion from status",
+			  FCVAR_SERVER_CAN_EXECUTE)
 {
 	if (context.GetPlayerSlot() != -1)
 		return;
@@ -222,7 +249,8 @@ CON_COMMAND_F(mm_removeexcludeslot, "Remove exclusion from status", FCVAR_SERVER
 	ConMsg("[StatusModifier] Removed slot %d from exclude list.\n", slot);
 }
 
-CON_COMMAND_F(mm_listexcludeslots, "List exclusion from status", FCVAR_SERVER_CAN_EXECUTE)
+CON_COMMAND_F(mm_listexcludeslots, "List exclusion from status",
+			  FCVAR_SERVER_CAN_EXECUTE)
 {
 	if (context.GetPlayerSlot() != -1)
 		return;
@@ -270,8 +298,11 @@ void ErrorLog(const char *msg, ...)
 	ConColorMsg(Color(255, 0, 0, 255), "[Error] %s\n", buf);
 
 	char szPath[256], szBuffer[2048];
-	g_SMAPI->PathFormat(szPath, sizeof(szPath), "%s/addons/logs/status-modifier-%s.txt", g_SMAPI->GetBaseDir(), formatCurrentTime2().c_str());
-	g_SMAPI->Format(szBuffer, sizeof(szBuffer), "L %s: %s\n", formatCurrentTime().c_str(), buf);
+	g_SMAPI->PathFormat(szPath, sizeof(szPath),
+						"%s/addons/logs/status-modifier-%s.txt",
+						g_SMAPI->GetBaseDir(), formatCurrentTime2().c_str());
+	g_SMAPI->Format(szBuffer, sizeof(szBuffer), "L %s: %s\n",
+					formatCurrentTime().c_str(), buf);
 
 	FILE *pFile = fopen(szPath, "a");
 	if (pFile)
@@ -282,42 +313,22 @@ void ErrorLog(const char *msg, ...)
 }
 
 ///////////////////////////////////////
-const char *StatusModifier::GetLicense()
-{
-	return "GPL";
-}
+const char *StatusModifier::GetLicense() { return "GPL"; }
 
-const char *StatusModifier::GetVersion()
-{
-	return "1.0.0";
-}
+const char *StatusModifier::GetVersion() { return "1.0.1"; }
 
-const char *StatusModifier::GetDate()
-{
-	return __DATE__;
-}
+const char *StatusModifier::GetDate() { return __DATE__; }
 
-const char *StatusModifier::GetLogTag()
-{
-	return "StatusModifier";
-}
+const char *StatusModifier::GetLogTag() { return "StatusModifier"; }
 
-const char *StatusModifier::GetAuthor()
-{
-	return "Cruze";
-}
+const char *StatusModifier::GetAuthor() { return "Cruze"; }
 
 const char *StatusModifier::GetDescription()
 {
-	return "Remove players you don't want to display in client console `status` command.";
+	return "Remove players you don't want to display in client console `status` "
+		   "command.";
 }
 
-const char *StatusModifier::GetName()
-{
-	return "Status Modifier";
-}
+const char *StatusModifier::GetName() { return "Status Modifier"; }
 
-const char *StatusModifier::GetURL()
-{
-	return "https://github.com/cruze03";
-}
+const char *StatusModifier::GetURL() { return "https://github.com/cruze03"; }
